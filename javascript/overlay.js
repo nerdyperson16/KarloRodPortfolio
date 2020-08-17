d23f7691c67ac99e8b8cd47e2666aba8ea4f1111
nerdyperson16 <67539794+nerdyperson16@users.noreply.github.com>
2020-08-17 01:11:30 -0700

    V2 for Karlo's Portfolio website
    This new update brings a new selection towards capabilities of the website. Instead of the original legacy website, new features such as the "hamburger menu" section and the "skip navigation" sections are added. To sum it all up, it's mostly just making the site more recent.

---
+++ javascript/overlay.js
@@ -0,0 +1,24 @@
+function on4() {
+    document.getElementById("nhf").style.display = "block";
+}
+function off4() {
+    document.getElementById("nhf").style.display = "none";
+}
+function on3() {
+    document.getElementById("nlh").style.display = "block";
+}
+function off3() {
+    document.getElementById("nlh").style.display = "none";
+}
+function on2() {
+    document.getElementById("kwkc").style.display = "block";
+}
+function off2() {
+    document.getElementById("kwkc").style.display = "none";
+}
+function on() {
+    document.getElementById("kcls").style.display = "block";
+}
+function off() {
+    document.getElementById("kcls").style.display = "none";
+}
